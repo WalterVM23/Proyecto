c136059596670dd5b94cedef8b69b8508ea0d28b
WalterVM23 <119743705+WalterVM23@users.noreply.github.com>
2023-05-29 17:28:05 -0600

    se arreglaron unos errores

---
+++ menu/menu.js
@@ -0,0 +1,31 @@
+contenidoMenu.innerHTML += ` 
+        <nav class="navbar navbar-expand-sm navbar-dark">
+              <div class="container">        
+                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="collapsibleNavId">
+                    <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="index.html" aria-current="page">Inicio <span class="visually-hidden">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="estudiante.html">estudiantes</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="profesor.html">profesores</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="Curso.html">cursos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="grupo.html">grupo</a>
+                        </li>           
+                    </ul>
+                    
+                </div>
+          </div>
+        </nav>
+  
+    `;
